0cc009e8ea6cd5a7081dc6661489ea0296337113
Aimery Methena <aimery.methena@gmail.com>
2020-06-12 13:46:08 -0500

    Updated the brawler's punch animation and added a camera movement script so that the camera follows the character (scene for GameObject with Player tag and is sticky towards looking at the ground.

---
+++ Scour the Depths/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	[Range (0,1)] public float cameraAcceleration;
+	public Vector3 offset;
+	public float cameraYStickyMod;
+	private Transform characterPosition;
+	private float characterBaseY;
+	private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        GameObject[] characterList = GameObject.FindGameObjectsWithTag("Player");
+		if(characterList.Length < 0)
+			Debug.LogError("CameraMovement script could not find an object with the character tag");
+		else
+			characterPosition = characterList[0].GetComponent<Transform>();
+		characterBaseY = characterPosition.position.y;
+    }
+
+    void FixedUpdate()
+    {
+		Vector3 targetPosition = new Vector3(characterPosition.position.x, (characterPosition.position.y * cameraYStickyMod + characterBaseY) / (1 + cameraYStickyMod), characterPosition.position.z);
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition + offset, ref velocity, cameraAcceleration);
+    }
+}
