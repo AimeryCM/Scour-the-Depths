d19b38ec058927bd6ed9305a7fb3dd29276b7c10
Aimery Methena <aimery.methena@gmail.com>
2020-06-18 01:31:29 -0500

    Adding everything that didn't add from InventoryUI

---
+++ Scour the Depths/Assets/Scripts/HotbarManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotbarManager : MonoBehaviour
+{
+	[SerializeField] private Image iconSpot;
+
+	public void UpdateIcon(Sprite icon)
+	{
+		iconSpot.enabled = true;
+		iconSpot.sprite = icon;
+	}
+}
