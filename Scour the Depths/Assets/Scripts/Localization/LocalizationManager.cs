0d1219a23196361eae582cca04c76c478639f38a
Aimery Methena <aimery.methena@gmail.com>
2020-07-02 14:55:34 -0500

    Added localization support so that localization can be used early on

---
+++ Scour the Depths/Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : MonoBehaviour
+{
+	public static LocalizationManager instance = null;
+
+	[SerializeField] private string missingText = "Localized Text not found";
+	private Dictionary<string, string> localizedText = null;
+	private bool isReady = false;
+
+	void Awake()
+	{
+		if(instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+		
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void LoadLocalizedText(string fileName)
+	{
+		localizedText = new Dictionary<string, string>();
+		string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+		if(File.Exists(filePath))
+		{
+			string dataAsJson = File.ReadAllText(filePath);
+			LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+			for(int x = 0; x < loadedData.items.Length; x++)
+			{
+				localizedText.Add(loadedData.items[x].key, loadedData.items[x].value);
+			}
+		} else
+		{
+			Debug.LogError("Localization file requested not found");
+		}
+		isReady = true;
+	}
+
+	public string GetLocalizedValue(string key)
+	{
+		return localizedText.ContainsKey(key) ? localizedText[key] : missingText;
+	}
+
+	public bool GetIsReady()
+	{
+		return isReady;
+	}
+}
---
+++ Scour the Depths/Assets/Scripts/Localization/LocalizedTextEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class LocalizedTextEditor : EditorWindow
+{
+	public LocalizationData localizationData = null;
+	Vector2 scrollPos = Vector2.zero;
+
+	[MenuItem("Window/Localized Text Editor")]
+	static void Init()
+	{
+		EditorWindow.GetWindow(typeof(LocalizedTextEditor)).Show();
+	}
+
+	private void OnGUI()
+	{
+		scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+		if(localizationData != null)
+		{
+			SerializedObject serializedObject = new SerializedObject(this);
+			SerializedProperty serializedProperty = serializedObject.FindProperty("localizationData");
+			EditorGUILayout.PropertyField(serializedProperty, true);
+			serializedObject.ApplyModifiedProperties();
+
+			if(GUILayout.Button("Save data"))
+			{
+				SaveGameData();
+			}
+		}
+		
+		if(GUILayout.Button("Load data"))
+			LoadGameData();
+		
+		if(GUILayout.Button("Create new data"))
+			CreateNewData();
+
+		EditorGUILayout.EndScrollView();
+	}
+
+	private void CreateNewData()
+	{
+		localizationData = new LocalizationData();
+	}
+
+	private void SaveGameData()
+	{
+		string filePath = EditorUtility.SaveFilePanel("Save localization data file", Application.streamingAssetsPath, "", "json");
+		if(!string.IsNullOrEmpty(filePath))
+		{
+			string dataAsJson = JsonUtility.ToJson(localizationData);
+			File.WriteAllText(filePath, dataAsJson);
+		}
+	}
+
+	private void LoadGameData()
+	{
+		string filePath = EditorUtility.OpenFilePanel("Select localization data file", Application.streamingAssetsPath, "json");
+		if(!string.IsNullOrEmpty(filePath))
+		{
+			string dataAsJson = File.ReadAllText(filePath);
+			localizationData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+		}
+	}
+}
